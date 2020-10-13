04131bbabb541ee666c7c32778c341b94b52df76
Paola Andrea Vergara Lopez <Paola@MacBook-Pro-de-Hansel.local>
2020-10-12 21:28:12 -0500

    conexion a la base de datos, correccion de estilos en el login y registro

--- resources/views/products/index.blade.php
+++
@@ -1,7 +0,0 @@
-@extends('layouts.app')
-
-@section('content')
-<div class="container">
-   <h1>para mostrale los products</h1>
-</div>
-@endsection
--- routes/web.php
+++ routes/web.php
@@ -13,3 +13,7 @@ Auth::routes();
 Route::get('/home', 'HomeController@index')->name('home');
 
 Route::get('/product', 'ProductController@index')->name('product');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
