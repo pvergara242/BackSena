5254f866ad64a0198fbfff764fce56a94f7472a1
Mm.44539934 <piolinfan07@gmail.com>
2020-10-16 15:47:06 -0500

    Instalación de turbolinks, and Table Roles, Permissions y products

--- database/migrations/2020_10_09_163736_create_role_user_table.php
+++ database/migrations/2020_10_09_163736_create_role_user_table.php
@@ -15,8 +15,19 @@ class CreateRoleUserTable extends Migration
     {
         Schema::create('role_user', function (Blueprint $table) {
             $table->bigIncrements('id');
+
+            $table->bigInteger('user_id')->unsigned();
+            $table->bigInteger('role_id')->unsigned();
             
             $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users')
+                ->onDelete('cascade')
+                ->onUpdate('cascade');
+
+            $table->foreign('role_id')->references('id')->on('roles')
+                ->onDelete('cascade')
+                ->onUpdate('cascade');
         });
     }
 
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,12 +14,39 @@ class DatabaseSeeder extends Seeder
     {
         // $this->call(UsersTableSeeder::class);
 
+        DB::table('roles')->insert([
+            'name' => 'superAdmin',
+            'description' => 'Administrador general del sistema',
+            'created_at' => '2020-10-09 13:42:02',
+            'updated_at' => '2020-10-09 13:42:08',
+        ]);
+        DB::table('roles')->insert([
+            'name' => 'admin',
+            'description' => 'admin del sistema',
+            'created_at' => '2020-10-09 13:42:10',
+            'updated_at' => '2020-10-09 13:42:14',
+        ]);
+        DB::table('roles')->insert([
+            'name' => 'usuario',
+            'description' => 'Usuario',
+            'created_at' => '2020-10-09 13:42:18',
+            'updated_at' => '2020-10-09 13:42:22',
+        ]);
+        DB::table('roles')->insert([
+            'name' => 'gerente',
+            'description' => 'Jefe de recursos',
+            'created_at' => '2020-10-09 13:42:26',
+            'updated_at' => '2020-10-09 13:42:32',
+        ]);
+
         DB::table('users')->insert([
             'name' => 'admin',
             'email' => 'admin@admin.com',
             'email_verified_at' => '2020-10-09 13:52:57',         
             'password' => bcrypt('123456'),
             'remember_token' => 'srsntr3423jhgs',
+            'created_at' => '2020-10-09 13:42:26',
+            'updated_at' => '2020-10-09 13:42:32',
         ]);
 
         DB::table('users')->insert([
@@ -28,6 +55,30 @@ class DatabaseSeeder extends Seeder
             'email_verified_at' => '2020-10-09 13:52:57',         
             'password' => bcrypt('123456'),
             'remember_token' => 'srsntr3fr423jhgs',
+            'created_at' => '2020-10-09 13:42:26',
+            'updated_at' => '2020-10-09 13:42:32',
+        ]);
+
+        DB::table('products')->insert([
+            'codigo' => '5423ngt',
+            'unidad' => '100',
+            'desripcion' => 'Zapatos para dama',         
+            'especificaciones' => 'De color Negro',         
+            'cantidad' => '30',
+            'productos' => 'Zapatos',
+            'created_at' => '2020-10-09 13:42:26',
+            'updated_at' => '2020-10-09 13:42:32',
+        ]);
+
+        DB::table('products')->insert([
+            'codigo' => '542gfngt',
+            'unidad' => '30',
+            'desripcion' => 'Carteras para dama',         
+            'especificaciones' => 'De color Negro',         
+            'cantidad' => '50',
+            'productos' => 'Carteras',
+            'created_at' => '2020-10-09 13:42:26',
+            'updated_at' => '2020-10-09 13:42:32',
         ]);
     }
 }
