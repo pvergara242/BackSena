599854e17516c60b90422367a7301e24b34cbb23
Paola Andrea Vergara Lopez <Paola@MacBook-Pro-de-Hansel.local>
2020-10-16 20:34:49 -0500

    probando

--- database/migrations/2020_10_16_152938_create_products_table.php
+++ database/migrations/2020_10_16_152938_create_products_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateRolesTable extends Migration
+class CreateProductsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,12 +13,16 @@ class CreateRolesTable extends Migration
      */
     public function up()
     {
-        Schema::create('roles', function (Blueprint $table) {
+        Schema::create('products', function (Blueprint $table) {
             $table->bigIncrements('id');
-            
-            $table->string('name');
-            $table->string('description');
 
+            $table->string('codigo');
+            $table->string('unidad');
+            $table->string('desripcion');
+            $table->string('especificaciones');
+            $table->string('cantidad');
+            $table->string('productos');
+            
             $table->timestamps();
         });
     }
@@ -30,6 +34,6 @@ class CreateRolesTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('roles');
+        Schema::dropIfExists('products');
     }
 }
