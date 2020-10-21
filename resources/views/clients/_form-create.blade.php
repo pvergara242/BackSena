4a1ed0779223e28df860e5b9d69c16444d10f289
Paola Andrea Vergara Lopez <Paola@MacBook-Pro-de-Hansel.local>
2020-10-21 17:15:05 -0500

    terminacion de formulario para registro de clientes

--- resources/views/clients/_form-create.blade.php
+++ resources/views/clients/_form-create.blade.php
@@ -73,6 +73,86 @@
       >
       <p class="text-danger text-center "><strong>{{ $errors->first('type') }}</strong></p>
    </div>
+   
+   <div class="inpForm">
+      <input class="form-control" 
+         type="text" 
+         name="razon_social" 
+         value="{{ old('razon_social') }}"
+         placeholder="Razon social"
+      >
+      <p class="text-danger text-center "><strong>{{ $errors->first('razon_social') }}</strong></p>
+   </div>
+   
+   <div class="inpForm">
+      <input class="form-control" 
+         type="text" 
+         name="pais_code" 
+         value="{{ old('pais_code') }}"
+         placeholder="Codigo País"
+      >
+      <p class="text-danger text-center "><strong>{{ $errors->first('pais_code') }}</strong></p>
+   </div>
+   
+   <div class="inpForm">
+      <input class="form-control" 
+         type="text" 
+         name="departamento_code" 
+         value="{{ old('departamento_code') }}"
+         placeholder="Codigo Departamento"
+      >
+      <p class="text-danger text-center "><strong>{{ $errors->first('departamento_code') }}</strong></p>
+   </div>
+   
+   <div class="inpForm">
+      <input class="form-control" 
+         type="text" 
+         name="municipio_code" 
+         value="{{ old('municipio_code') }}"
+         placeholder="Codigo Municipio"
+      >
+      <p class="text-danger text-center "><strong>{{ $errors->first('municipio_code') }}</strong></p>
+   </div>
+   
+   <div class="inpForm">
+      <input class="form-control" 
+         type="text" 
+         name="address" 
+         value="{{ old('address') }}"
+         placeholder="Dirección"
+      >
+      <p class="text-danger text-center "><strong>{{ $errors->first('address') }}</strong></p>
+   </div>
+   
+   <div class="inpForm">
+      <input class="form-control" 
+         type="text" 
+         name="phone" 
+         value="{{ old('phone') }}"
+         placeholder="Telefono"
+      >
+      <p class="text-danger text-center "><strong>{{ $errors->first('phone') }}</strong></p>
+   </div>
+   
+   <div class="inpForm">
+      <input class="form-control" 
+         type="text" 
+         name="phone" 
+         value="{{ old('phone') }}"
+         placeholder="Telefono"
+      >
+      <p class="text-danger text-center "><strong>{{ $errors->first('phone') }}</strong></p>
+   </div>
+   
+   <div class="inpForm">
+      <input class="form-control" 
+         type="text" 
+         name="email" 
+         value="{{ old('email') }}"
+         placeholder="Correo electronico"
+      >
+      <p class="text-danger text-center "><strong>{{ $errors->first('email') }}</strong></p>
+   </div>
 
 
    
