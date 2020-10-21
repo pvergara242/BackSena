50874cb6907340b7df6dcfa95afa6c7bfee3a928
Paola Andrea Vergara Lopez <Paola@MacBook-Pro-de-Hansel.local>
2020-10-21 17:29:52 -0500

    terminado

---
+++ resources/views/products/create.blade.php
@@ -0,0 +1,32 @@
+@extends('layouts.panel')
+
+
+<main class="dashboard">
+   <div class="childDashboard">
+
+      <section class="navSuperior">
+      @include('components/navSuperior')        
+      </section>
+            
+      <section class="DashboardBody">
+         
+         <div class=" sidebar">
+            @include('components.sidebar')
+         </div>
+
+         <main class="ctnBody">
+            <h1 class="text-center">Agregar Productos</h1>
+            @include('clients._form-create')
+
+         </main>
+
+      </section>
+
+   </div>   
+
+</main>
+
+<main class="footer">
+   @include('components.footer')
+</main>
+
