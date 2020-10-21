1b2fde9113c4b9ec2c162ef0cd7959db32c342ce
Mm.44539934 <piolinfan07@gmail.com>
2020-10-21 17:03:27 -0500

    create form add client

--- resources/views/clients/_form-create.blade.php
+++ resources/views/clients/_form-create.blade.php
@@ -1,48 +1,82 @@
 @include('partials.sessions_status')
 
-<div class="row">
-   <div class="col-lg-12">
-      <form action="" method="POST" enctype="multipart/form-data">
+<form class="formulario" action="" method="POST" enctype="multipart/form-data">
 
-         @csrf
-      
-         <div class="col-md-6">
-            <input class="form-control mt-4" 
-               type="number" 
-               name="document" 
-               value="{{ old('document') }}"
-               placeholder="Documento"
-            >
-            <p class="text-danger text-center "><strong>{{ $errors->first('document') }}</strong></p>
-         </div>
-         <div class="col-md-6">
-            <input class="form-control mt-4" 
-               type="number" 
-               name="document" 
-               value="{{ old('document') }}"
-               placeholder="Documento"
-            >
-            <p class="text-danger text-center "><strong>{{ $errors->first('document') }}</strong></p>
-         </div>
-         <div class="col-md-6">
-            <input class="form-control mt-4" 
-               type="number" 
-               name="document" 
-               value="{{ old('document') }}"
-               placeholder="Documento"
-            >
-            <p class="text-danger text-center "><strong>{{ $errors->first('document') }}</strong></p>
-         </div>
-         <div class="col-md-6">
-            <input class="form-control mt-4" 
-               type="number" 
-               name="document" 
-               value="{{ old('document') }}"
-               placeholder="Documento"
-            >
-            <p class="text-danger text-center "><strong>{{ $errors->first('document') }}</strong></p>
-         </div>
-      
-      </form>
+   @csrf
+
+   <div class="inpForm">
+      <input class="form-control" 
+         type="number" 
+         name="document" 
+         value="{{ old('document') }}"
+         placeholder="Documento"
+      >
+      <p class="text-danger text-center "><strong>{{ $errors->first('document') }}</strong></p>
    </div>
-</div>
+
+   <div class="inpForm">
+      {{-- <input class="form-control" 
+         type="number" 
+         name="document" 
+         value="{{ old('document') }}"
+         placeholder="Documento"
+      >
+      <p class="text-danger text-center "><strong>{{ $errors->first('document') }}</strong></p> --}}
+   </div>
+
+   <div class="inpForm">
+      <input class="form-control" 
+         type="text" 
+         name="name" 
+         value="{{ old('name') }}"
+         placeholder="Primer Nombre"
+      >
+      <p class="text-danger text-center "><strong>{{ $errors->first('name') }}</strong></p>
+   </div>
+
+   <div class="inpForm">
+      <input class="form-control" 
+         type="text" 
+         name="last_name" 
+         value="{{ old('last_name') }}"
+         placeholder="Segundo Nombre"
+      >
+      <p class="text-danger text-center "><strong>{{ $errors->first('last_name') }}</strong></p>
+   </div>
+
+   <div class="inpForm">
+      <input class="form-control" 
+         type="text" 
+         name="surname" 
+         value="{{ old('surname') }}"
+         placeholder="Primer apellido"
+      >
+      <p class="text-danger text-center "><strong>{{ $errors->first('surname') }}</strong></p>
+   </div>
+
+   <div class="inpForm">
+      <input class="form-control" 
+         type="text" 
+         name="last_surname" 
+         value="{{ old('last_surname') }}"
+         placeholder="Segundo apellido"
+      >
+      <p class="text-danger text-center "><strong>{{ $errors->first('last_surname') }}</strong></p>
+   </div>
+
+   <div class="inpForm">
+      <input class="form-control" 
+         type="text" 
+         name="type" 
+         value="{{ old('type') }}"
+         placeholder="Tipo de usuario"
+      >
+      <p class="text-danger text-center "><strong>{{ $errors->first('type') }}</strong></p>
+   </div>
+
+
+   
+
+   
+
+</form>
