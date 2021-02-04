99468fe363c309d7e6da35ec26f104e36c63415f
Paola Andrea Vergara Lopez <Paola@MacBook-Pro-de-Hansel.local>
2021-02-04 11:22:58 -0500

    correccion salida de productos

--- app/ProductoSalida.php
+++ app/ProductoSalida.php
@@ -6,5 +6,26 @@ use Illuminate\Database\Eloquent\Model;
 
 class ProductoSalida extends Model
 {
-    //
-}
+    protected $fillable = [
+        'fechaSolicitud', 
+        'area', 
+        'codigoRegional',
+        'nombreRegional',
+        'codigoCentroCostos',
+        'nombreCentroCostos',
+        'cordinadorArea',
+        'numeroDocumento',
+        'nombreServidorPublico',
+        'numeroDocumentoServidor',
+        'codigoFichaCaracterizacion',
+        'codigoSena',
+        'descripcionBien',
+        'unidadMedida',
+        'cantidadSolicitada',
+        'cantidadEntregada',
+        'observaciones',
+        'nombre',
+        'cargo',
+        
+    ];
+}   
