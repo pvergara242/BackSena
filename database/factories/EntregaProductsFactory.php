02abe6bf084b6ecb1686acebf95151b60795cc82
dev_josefandino <piolinfan@gmail.com>
2021-03-04 18:04:29 -0500

    EntregaProducts

---
+++ database/factories/EntregaProductsFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\EntregaProducts;
+use Faker\Generator as Faker;
+
+$factory->define(EntregaProducts::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
